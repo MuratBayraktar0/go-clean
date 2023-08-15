1d166660dbfa35b730775bada0347c3c1f627b73
murat <murat@bayraktar.com>
2023-08-15 05:24:15 +0300

    Add the usecase of the user in the application layer

---
+++ application/user-usecase.go
@@ -0,0 +1,84 @@
+package application
+
+import (
+	"context"
+	"golang-clean/domain"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type userUsecase struct {
+	userRepo       domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewUserUsecase(userRepo domain.UserRepository, timeout time.Duration) *userUsecase {
+	return &userUsecase{
+		userRepo:       userRepo,
+		contextTimeout: timeout,
+	}
+}
+
+func (u *userUsecase) Get(ctx context.Context, id string) (*domain.UserDTO, error) {
+	userEntity, err := u.userRepo.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	userDTO := ConvertEntitytoDTO(*userEntity)
+	return &userDTO, nil
+}
+
+func (u *userUsecase) GetAll(ctx context.Context) (*[]domain.UserDTO, error) {
+	usersEntity, err := u.userRepo.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	usersDTO := []domain.UserDTO{}
+	for _, userEntity := range *usersEntity {
+		usersDTO = append(usersDTO, ConvertEntitytoDTO(userEntity))
+	}
+
+	return &usersDTO, nil
+}
+
+func (u *userUsecase) Create(ctx context.Context, user domain.UserDTO) error {
+	userEntity := ConvertDTOtoEntity(user)
+	userEntity.ID = uuid.New().String()
+	userEntity.UpdatedAt = time.Now()
+	userEntity.CreatedAt = time.Now()
+	err := u.userRepo.Create(ctx, userEntity)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *userUsecase) Update(ctx context.Context, id string, user domain.UserDTO) error {
+	userEntity := ConvertDTOtoEntity(user)
+	userEntity.ID = id
+	userEntity.UpdatedAt = time.Now()
+	err := u.userRepo.Update(ctx, userEntity)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ConvertEntitytoDTO(userEntity domain.UserEntity) domain.UserDTO {
+	return domain.UserDTO{
+		Name:  userEntity.Name,
+		Email: userEntity.Email,
+	}
+}
+
+func ConvertDTOtoEntity(userDTO domain.UserDTO) domain.UserEntity {
+	return domain.UserEntity{
+		Name:  userDTO.Name,
+		Email: userDTO.Email,
+	}
+}
---
+++ application/user-usecase_test.go
@@ -0,0 +1,130 @@
+package application
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"golang-clean/domain"
+)
+
+// Mock repository for user
+type mockUserRepo struct {
+	mock.Mock
+}
+
+func (m *mockUserRepo) Get(ctx context.Context, id string) (*domain.UserEntity, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(*domain.UserEntity), args.Error(1)
+}
+
+func (m *mockUserRepo) GetAll(ctx context.Context) (*[]domain.UserEntity, error) {
+	args := m.Called(ctx)
+	return args.Get(0).(*[]domain.UserEntity), args.Error(1)
+}
+
+func (m *mockUserRepo) Create(ctx context.Context, user domain.UserEntity) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (m *mockUserRepo) Update(ctx context.Context, user domain.UserEntity) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+// ... Implement other repository methods ...
+
+func TestUsecase_Get(t *testing.T) {
+	mockRepo := &mockUserRepo{}
+
+	userID := uuid.New().String()
+	mockEntity := &domain.UserEntity{
+		ID:        userID,
+		Name:      "John Doe",
+		Email:     "john@example.com",
+		UpdatedAt: time.Now(),
+		CreatedAt: time.Now(),
+	}
+
+	mockRepo.On("Get", mock.Anything, userID).Return(mockEntity, nil)
+
+	usecase := NewUserUsecase(mockRepo, time.Second*5)
+	ctx := context.Background()
+
+	result, err := usecase.Get(ctx, userID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, mockEntity.Name, result.Name)
+	assert.Equal(t, mockEntity.Email, result.Email)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUsecase_GetAll(t *testing.T) {
+	mockRepo := &mockUserRepo{}
+
+	mockUsers := []domain.UserEntity{
+		{ID: "1", Name: "John Doe", Email: "john@example.com", UpdatedAt: time.Now(), CreatedAt: time.Now()},
+		{ID: "2", Name: "Jane Doe", Email: "jane@example.com", UpdatedAt: time.Now(), CreatedAt: time.Now()},
+	}
+
+	mockRepo.On("GetAll", mock.Anything).Return(&mockUsers, nil)
+
+	usecase := NewUserUsecase(mockRepo, time.Second*5)
+	ctx := context.Background()
+
+	results, err := usecase.GetAll(ctx)
+
+	assert.NoError(t, err)
+	assert.Len(t, *results, len(mockUsers))
+
+	for i, user := range *results {
+		assert.Equal(t, mockUsers[i].Name, user.Name)
+		assert.Equal(t, mockUsers[i].Email, user.Email)
+	}
+
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUsecase_Create(t *testing.T) {
+	mockRepo := &mockUserRepo{}
+	mockRepo.On("Create", mock.Anything, mock.AnythingOfType("domain.UserEntity")).Return(nil)
+
+	usecase := NewUserUsecase(mockRepo, time.Second*5)
+	ctx := context.Background()
+
+	userDTO := domain.UserDTO{
+		Name:  "Jane Doe",
+		Email: "jane@example.com",
+	}
+
+	err := usecase.Create(ctx, userDTO)
+
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+
+}
+
+func TestUsecase_Update(t *testing.T) {
+	mockRepo := &mockUserRepo{}
+
+	userID := uuid.New().String()
+	userDTO := domain.UserDTO{
+		Name:  "Updated Name",
+		Email: "updated@example.com",
+	}
+
+	mockRepo.On("Update", mock.Anything, mock.AnythingOfType("domain.UserEntity")).Return(nil)
+
+	usecase := NewUserUsecase(mockRepo, time.Second*5)
+	ctx := context.Background()
+
+	err := usecase.Update(ctx, userID, userDTO)
+
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+}
